6aaf8fa51f4e34ca718ec2870dd0ead6ec5712cd
xujiemou <xujiemou@sunline.cn>
2022-03-31 17:49:59 +0800

    还款计划提交

--- modules/lib-repayment-schedule/src/main/kotlin/cn/sunline/saas/repayment/schedule/model/dto/DTOPrincipalCalculator.kt
+++ modules/lib-repayment-schedule/src/main/kotlin/cn/sunline/saas/repayment/schedule/model/dto/DTOPrincipalCalculator.kt
@@ -2,8 +2,8 @@
 
 package cn.sunline.saas.repayment.schedule.model.dto
 
-import cn.sunline.saas.global.constant.PaymentMethodType
-import cn.sunline.saas.global.constant.RepaymentFrequency
+import cn.sunline.saas.repayment.model.PaymentMethodType
+import cn.sunline.saas.repayment.model.RepaymentFrequency
 import java.math.BigDecimal
 
 
