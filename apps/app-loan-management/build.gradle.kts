5382f8a73d9d5ed9e3e3d56976c4d44e19fb3d36
xujiemou <xujiemou@sunline.cn>
2022-04-18 10:14:25 +0800

    配置注释

--- apps/app-loan-management/build.gradle.kts
+++ apps/app-loan-management/build.gradle.kts
@@ -19,6 +19,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${jacksonVersion}")
     implementation("joda-time:joda-time:${jodaVersion}")
 
+
+//    implementation("com.googlecode.log4jdbc:log4jdbc:1.2")
     implementation(project(":lib-exceptions"))
     implementation(project(":lib-response"))
     implementation(project(":lib-menu"))
