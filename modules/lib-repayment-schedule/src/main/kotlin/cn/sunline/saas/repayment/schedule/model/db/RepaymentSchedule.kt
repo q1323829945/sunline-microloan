8d72227ee095ce4ae2e38c142f41c78474392c67
xujiemou <xujiemou@sunline.cn>
2022-04-25 15:30:53 +0800

    修改合并报错

--- modules/lib-repayment-schedule/src/main/kotlin/cn/sunline/saas/repayment/schedule/model/db/RepaymentSchedule.kt
+++ modules/lib-repayment-schedule/src/main/kotlin/cn/sunline/saas/repayment/schedule/model/db/RepaymentSchedule.kt
@@ -22,32 +22,16 @@ class RepaymentSchedule(
     var id: Long, // id
 
     @NotNull
-    @Column(name = "installment", precision = 15, scale = 2, nullable = false)
     @Column(name = "installment", precision = 15, scale = 2, nullable = false, columnDefinition = "decimal(15,2) not null")
-
-
     var installment: BigDecimal,// 贷款金额 installment
 
     @NotNull
-    @Column(name = "interest_Rate", precision = 9, scale = 6, nullable = false)
     @Column(name = "interest_Rate", precision = 9, scale = 6, nullable = false, columnDefinition = "decimal(9,6) not null")
-
-
-
-
-
-
-
-
-
     var interestRate: BigDecimal, // 贷款利率
 
     @NotNull
-    @Column(name = "term", nullable = false)
-
     @Column(name = "term", nullable = false, length = 32, columnDefinition = "varchar(32) not null")
     @Enumerated(value = EnumType.STRING)
-
     var term: LoanTermType, // 贷款期限
 
     @OneToMany(fetch = FetchType.EAGER, cascade = [CascadeType.ALL])
